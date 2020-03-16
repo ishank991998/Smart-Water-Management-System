ec660f5b4b2be87d8e73d7df363680e964c0d07b
ishank991998 <ishank991998@gmail.com>
2020-03-16 12:21:06 +0530

    Linked Database

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    database: 'mongodb://localhost:27017/meanauth',
+    database: 'mongodb+srv://yid:YID2020@smart-water-management-system-eci2r.mongodb.net/test?retryWrites=true&w=majority',
     secret: 'yoursecret'
 }
