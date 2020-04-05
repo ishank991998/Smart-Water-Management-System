9ba6d08de1a6cd4e25445a1d21082970ef8aec21
ishank991998 <ishank991998@gmail.com>
2020-04-05 11:41:38 +0530

    Login/Register on Home page resolved

--- angular-src/src/app/services/auth.service.ts
+++ angular-src/src/app/services/auth.service.ts
@@ -15,7 +15,7 @@ export class AuthService {
   registerUser(user){
     let headers = new Headers();
     headers.append('Content-Type','application/json');
-    return this.http.post('http://localhost:3000/users/register',user,{headers: headers})
+    return this.http.post('users/register',user,{headers: headers})
     .map(res => res.json());
   }
 
@@ -25,7 +25,7 @@ export class AuthService {
     headers.append('Content-Type','application/json');
     //console.log(headers);
     //console.log(this.http.post('http://localhost:3000/users/authenticate',user,{headers: headers}))
-    return this.http.post('http://localhost:3000/users/authenticate',user,{headers: headers})
+    return this.http.post('users/authenticate',user,{headers: headers})
     .map(res => res.json());
   }
 
@@ -34,7 +34,7 @@ export class AuthService {
     this.loadToken();
     headers.append('Authorization',this.authToken);
     headers.append('Content-Type','application/json');
-    return this.http.get('http://localhost:3000/users/profile',{headers: headers})
+    return this.http.get('users/profile',{headers: headers})
     .map(res => res.json());
   }
 
@@ -69,7 +69,7 @@ export class AuthService {
     let headers = new Headers();
     headers.append('Content-Type','application/json');
     console.log(data);
-    return this.http.post('http://localhost:3000/users/dashboard',data,{headers: headers})
+    return this.http.post('users/dashboard',data,{headers: headers})
     .map(res => res.json());
   }
 }
--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
-    database: 'mongodb://localhost:27017/meanauth', //'mongodb+srv://yid:YID2020@smart-water-management-system-eci2r.mongodb.net/test?retryWrites=true&w=majority',
-       
+    database:  'mongodb+srv://yid:YID2020@smart-water-management-system-eci2r.mongodb.net/test?retryWrites=true&w=majority',
+       // 'mongodb://localhost:27017/meanauth',
     secret: 'yoursecret'
 }
